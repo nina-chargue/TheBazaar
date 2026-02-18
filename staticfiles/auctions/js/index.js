ea9f54556653c18d9445f9c264af09d7fd74878b
nina-chargue <nina8chargue@gmail.com>
2026-02-18 00:42:23 -0600

    Remove local environment files from repo

--- staticfiles/auctions/js/index.js
+++
@@ -1,9 +0,0 @@
-function filterCategory(button) {
-    var buttons = document.querySelectorAll('.category-filter');
-    buttons.forEach(function(btn) {
-        btn.classList.remove('active');
-    });
-    button.classList.add('active');
-    document.getElementById('category-input').value = button.dataset.value;
-    document.getElementById('category-form').submit(); // Submit the form
-}
